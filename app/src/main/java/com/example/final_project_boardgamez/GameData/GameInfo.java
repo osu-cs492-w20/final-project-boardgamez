a3b0b03725d2a0129f285997bd9dcb1ed2bcb555
Carson Pemble <43591853+PembleC@users.noreply.github.com>
2020-02-29 13:14:59 -0800

    Adding API stuff
    Added:
    GameData\GameInfo.java
    Utilitlies\BoardGameGeek.java
    
    GameInfo will hold the data from the XML returned from API call
    
    BoardGameGeek builds the URL

---
+++ app/src/main/java/com/example/final_project_boardgamez/GameData/GameInfo.java
@@ -0,0 +1,16 @@
+package com.example.final_project_boardgamez.GameData;
+
+import java.io.Serializable;
+
+public class GameInfo implements Serializable {
+
+    public int yearpublished;
+    public int minplayers;
+    public int maxplayers;
+    public int playingtime;
+    public int age;
+
+    public String name;
+    public String description;
+
+}
---
+++ app/src/main/java/com/example/final_project_boardgamez/Utilitlies/BoardGameGeek.java
@@ -0,0 +1,27 @@
+package com.example.final_project_boardgamez.Utilitlies;
+
+import android.net.Uri;
+
+import com.example.final_project_boardgamez.GameData.GameInfo;
+
+import java.util.ArrayList;
+
+public class BoardGameGeek {
+    private final static String BoardGameGeek_SEARCH_BASE_URL = "http://www.boardgamegeek.com/xmlapi/";
+    private final static String BoardGameGeek_SEARCH_QUERY_PARAM = "search";
+
+    static class GitHubSearchResults {
+        ArrayList<GameInfo> games;
+    }
+
+    public static String buildGitHubSearchURL(String userGame) {
+        return Uri.parse(BoardGameGeek_SEARCH_BASE_URL).buildUpon()
+                .appendQueryParameter(BoardGameGeek_SEARCH_QUERY_PARAM, userGame)
+                .build()
+                .toString();
+    }
+}
+
+
+// Example API Call
+//http://www.boardgamegeek.com/xmlapi/search?search=Crossbows%20and%20Catapults
