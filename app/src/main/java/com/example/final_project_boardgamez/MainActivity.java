43fec438b9b48b345febcc18eb72fcc1e51c4c76
Terri Hewitt <hewittt@oregonstate.edu>
2020-02-29 23:18:56 -0800

    Add filter alert dialog

--- app/src/main/java/com/example/final_project_boardgamez/MainActivity.java
+++ app/src/main/java/com/example/final_project_boardgamez/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.final_project_boardgamez;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,6 +8,7 @@ import com.example.final_project_boardgamez.GameData.GameInfo;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,6 +19,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements GameManagerAdapter.OnGameClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -24,6 +29,10 @@ public class MainActivity extends AppCompatActivity implements GameManagerAdapte
     private RecyclerView mMainGameListRV;
     private GameManagerAdapter mAdapterRV;                  // Changed to a custom adapter
     private RecyclerView.LayoutManager mLayoutManagerRV;
+    private TextView mAppliedFiltersTV;
+    private String[] mFilterItems;
+    boolean[] mCheckedFilters;
+    ArrayList<Integer> mSelectedFilters = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +42,11 @@ public class MainActivity extends AppCompatActivity implements GameManagerAdapte
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mFilterItems = getResources().getStringArray(R.array.filter_list);
+        mCheckedFilters = new boolean[mFilterItems.length];
 
+        mAppliedFiltersTV = findViewById(R.id.tv_applied_filters);
+        mAppliedFiltersTV.setVisibility(View.GONE);
         /*ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_add_sign);
@@ -74,7 +87,8 @@ public class MainActivity extends AppCompatActivity implements GameManagerAdapte
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_filter) {
+            onFilterSettingsClicked();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -89,5 +103,70 @@ public class MainActivity extends AppCompatActivity implements GameManagerAdapte
         Intent intent = new Intent(this, GameDetailedActivity.class);
         //intent.putExtra(GameDetailedActivity.EXTRA_GAME_INFO, game);
         startActivity(intent);
+    }
+
+    private void onFilterSettingsClicked() {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        mBuilder.setTitle("Filters");
+        mBuilder.setMultiChoiceItems(mFilterItems, mCheckedFilters, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+                if (isChecked) {
+                    if (!mSelectedFilters.contains(position)){
+                        mSelectedFilters.add(position);
+                    }
+                } else { // Filter was unchecked
+                    if (mSelectedFilters.contains(position)) {
+                        mSelectedFilters.remove(position);
+                    }
+                }
+            }
+        });
+
+        mBuilder.setCancelable(false);
+        mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                String filterItem = "";
+                if (mSelectedFilters.size() > 0) {
+                    for (int i = 0; i < mSelectedFilters.size(); i++) {
+                        filterItem = filterItem + mFilterItems[mSelectedFilters.get(i)];
+                        if (i != mSelectedFilters.size() - 1) {
+                            filterItem = filterItem + ", ";
+                        }
+                        Log.d(TAG, filterItem);
+                    }
+                    mAppliedFiltersTV.setText("Tag filters: " + filterItem);
+                    mAppliedFiltersTV.setVisibility(View.VISIBLE);
+                    // Set text here
+                } else {
+                    mAppliedFiltersTV.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // TODO: Decide if we want a dismiss button
+          /*  mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }); */
+
+        mBuilder.setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                for (int i = 0; i < mCheckedFilters.length; i++) {  // Loop through checked items
+                    mCheckedFilters[i] = false;
+                    mSelectedFilters.clear();
+                    mAppliedFiltersTV.setText("");
+                    mAppliedFiltersTV.setVisibility(View.GONE);
+                    // Clear text view if any
+                }
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
     }
 }
