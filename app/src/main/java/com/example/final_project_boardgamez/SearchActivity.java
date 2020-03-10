1111f1410264eb08d9ea56f2b7ed7365359cfcf5
orourtim <orourtim@oregonstate.edu>
2020-03-10 16:47:02 -0700

    Created SearchActivity

---
+++ app/src/main/java/com/example/final_project_boardgamez/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.final_project_boardgamez;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
